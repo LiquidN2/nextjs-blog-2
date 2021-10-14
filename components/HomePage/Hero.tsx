4e5efda31b3c6aa8611939a32cd1affd3ddc210a
Hugh Nguyen <hhnguyen255@gmail.com>
2021-10-14 14:02:27 +1100

    completed the Hero component

--- components/HomePage/Hero.tsx
+++ components/HomePage/Hero.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import Image from 'next/image';
+
 import styles from './Hero.module.scss';
 
 const Hero: React.FC = () => {
-  return <div>Hero Component</div>;
+  return (
+    <section className={styles.hero}>
+      <div className={styles.image}>
+        <Image
+          src="/img/site/hugh.jpg"
+          alt="an image showing Hugh"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I am Hugh</h1>
+      <p>
+        I blog about web development - especially frontend framework like React
+        and Vue
+      </p>
+    </section>
+  );
 };
 
 export default Hero;
